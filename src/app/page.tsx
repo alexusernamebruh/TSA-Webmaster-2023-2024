d0f905f1bf85db895458ccb03c6a5588471e947c
alexusernamebruh <85073684+alexusernamebruh@users.noreply.github.com>
2023-12-29 14:20:11 -0500

    introadd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,46 @@
 import Image from 'next/image';
 import treesvg from './images/tree.svg';
+import { useState } from 'react';
+import Typewriter from './components/typewriter';
+
 export default function Home() {
   return (
     //Theme colors are grass green, sky blue, and white
-    <div className='bg-black h-screen w-screen comeInFocus'>
-      <div className="bg-[url('/Sunrise.jpeg')] fadeToBlack bg-fixed overflow-clip w-full h-full bg-cover bg-no-repeat">
-        <div className='flex'>
-          {/* add animated sparkles to background*/}
-          {/* have clouds from bottom come in for scene change once text is done with animation*/}
-          <div className='flex comeFromBottom cursive space-x-4 w-full p-8 justify-center text-6xl text-white font-bold'>
-            <p className='textBounce1'>Would</p>
-            <p className='textBounce2'>a</p>
-            <p className='textBounce3'>world</p>
-            <p className='textBounce4'>with</p>
-            <p className='textBounce5'>only</p>
-            <p className='textBounce6'>renewable</p>
-            <p className='textBounce7'>power</p>
-            <p className='textBounce8'>be</p>
-            <p className='textBounce9'>nice?</p>
+    <div className='bg-gray-900 h-screen w-screen z-40 flex flex-col'>
+      <div className='flex bg-gradient-to-br bg-gray-900 h-[7vh] z-40'>
+        <div className='my-auto p-4'>
+          <div>
+            <img
+              className='loadingAnimationSolarPanel w-12 rounded-full h-12 shadow-xl'
+              src='https://www.svgrepo.com/show/236459/solar-energy-solar-panel.svg'
+            />
           </div>
         </div>
-        <div className='cloudsSceneChange opacity-0'>
-          <img src='/cloudsTransparent.png' />
+      </div>
+
+      <div className='w-full h-full bg-grid flex'>
+        <img
+          className='w-full h-[93vh] opacity-70 z-0 bgFadeIn absolute'
+          src='https://cdn.pixabay.com/photo/2017/03/13/07/34/photovoltaic-2138992_1280.jpg'
+        />
+        <div className='mx-auto my-auto text-center z-40'>
+          <div>
+            <p className=' text-5xl font-extrabold'>
+              <span className='bg-gradient-to-r from-green-400 to-lime-500 text-transparent bg-clip-text textIntro1'>
+                Green
+              </span>
+              <span className='textIntro2 bg-gradient-to-r from-yellow-200 via-yellow-400 to-yellow-500 text-transparent bg-clip-text'>
+                {' '}
+                Energy
+              </span>
+            </p>
+            <p className='textIntro3 text-3xl pt-2 font-bold text-white'>
+              The best for our world
+            </p>
+            {/* <p className='text-5xl font-extrabold text-transparent  bg-clip-text textIntro bg-gradient-to-r from-green-400 to-lime-500'>
+              Solar Panel
+            </p> */}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/app/test.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import treesvg from './images/tree.svg';
+import { useState } from 'react';
+import Typewriter from './components/typewriter';
+
+export default function Home() {
+  return (
+    //Theme colors are grass green, sky blue, and white
+    <div className='bg-black h-screen w-screen'>
+      <div className="bg-[url('/Sunrise.jpeg')] scale fadeToBlack bg-fixed overflow-clip w-full h-full bg-cover bg-no-repeat">
+        <div className='flex'>
+          <div>
+            <img
+              src='https://clipart-library.com/img1/1663288.gif'
+              className='absolute w-10 h-10 birdFlying'
+            />
+            <img
+              src='https://clipart-library.com/img1/1663288.gif'
+              className='absolute w-10 h-10 -ml-[2rem] birdFlying'
+            />{' '}
+            <div>
+              <img
+                src='https://clipart-library.com/img1/1663288.gif'
+                className='absolute w-10 h-10 mt-[2rem] -ml-[0.5rem] birdFlying'
+              />
+              <img
+                src='https://clipart-library.com/img1/1663288.gif'
+                className='absolute w-10 h-10 mt-[4rem] -ml-[2rem] birdFlying'
+              />{' '}
+              <img
+                src='https://clipart-library.com/img1/1663288.gif'
+                className='absolute w-10 h-10 mt-[5.8rem] -ml-[3.5rem] birdFlying'
+              />
+            </div>
+            <img
+              src='https://clipart-library.com/img1/1663288.gif'
+              className='absolute w-10 h-10 -ml-[4rem] birdFlying'
+            />{' '}
+            <img
+              src='https://clipart-library.com/img1/1663288.gif'
+              className='absolute w-10 h-10 -ml-[6rem] birdFlying'
+            />
+            <img
+              src='https://clipart-library.com/img1/1663288.gif'
+              className='absolute w-10 h-10 -ml-[8rem] birdFlying'
+            />
+          </div>
+          {/* have text be typewriter effect before bounce*/}
+          {/* have clouds from bottom come in for scene change once text is done with animation*/}
+          {/* <Typewriter text={['AmongUs', 'ez', 'eae']} delay={200} infinite /> */}
+          <div className='flex comeFromBottom cursive space-x-4 w-full p-8 justify-center text-6xl text-white font-bold'>
+            <p className='textBounce1'>Would</p>
+            <p className='textBounce2'>a</p>
+            <p className='textBounce3'>world</p>
+            <p className='textBounce4'>with</p>
+            <p className='textBounce5'>only</p>
+            <p className='textBounce6'>renewable</p>
+            <p className='textBounce7'>power</p>
+            <p className='textBounce8'>be</p>
+            <p className='textBounce9'>nice?</p>
+          </div>
+        </div>
+        <div className='cloudsSceneChange opacity-0'>
+          <img src='/cloudsTransparent.png' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
